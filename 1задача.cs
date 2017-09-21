eaea0240e5681b7f8bbaf13f05668fad2abf31c1
IslamKarakulov <31709279+IslamKarakulov@users.noreply.github.com>
2017-09-21 12:51:14 +0300

    Add files via upload

---
+++ 1задача.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите х: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ответ: ");
+            Console.WriteLine(2 * x * 134 * x - x * x);
+            Console.ReadKey();
+        }
+    }
+}
