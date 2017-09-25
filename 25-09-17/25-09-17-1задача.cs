6ffa43b48cc671786e6cd75077200e02d076862a
IslamKarakulov <31709279+IslamKarakulov@users.noreply.github.com>
2017-09-25 13:11:05 +0300

    Add files via upload

---
+++ 25-09-17/25-09-17-1задача.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов в массиве:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i<n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int max = array[0];
+            for (int i = 0; i < n; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }   
+    }
+}
---
+++ 25-09-17/25-09-17-3задача.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов в массиве:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int plus = 0;
+            int minus = 0;
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i <n; i++)
+            {
+                if (array[i] > 0)
+                {
+                    plus++;
+                }
+                else if (array[i] < 0) 
+                {
+                    minus ++;
+                }
+            }
+            Console.WriteLine("Положительных: " + plus + ", отрицательных: " + minus);
+            Console.ReadKey();
+        }
+    }
+}
